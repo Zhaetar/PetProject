2fd159b03a110f8594fedea2291ebcb1694b5070
Zhaetar <zhaetar@gmail.com>
2017-11-12 21:26:46 -0200

    Atualização

--- app/src/main/java/jessicamatheus/petproject/HomeActivity.java
+++ app/src/main/java/jessicamatheus/petproject/HomeActivity.java
@@ -1,18 +1,25 @@
 package jessicamatheus.petproject;
 
 import android.app.Activity;
+import android.app.AlarmManager;
 import android.app.AlertDialog;
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.SystemClock;
 import android.provider.MediaStore;
+import android.support.v4.app.NotificationCompat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -24,6 +31,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Calendar;
 
 
 public class HomeActivity extends Activity{
@@ -51,8 +59,9 @@ public class HomeActivity extends Activity{
                 //sendToast("Notificação criada com sucesso.");
                 //setContentView(R.layout.petinterface);
                 //showAlertDialog();
-                Intent intent = new Intent(HomeActivity.this, DebugInterface.class);
-                startActivity(intent);
+                scheduleNotification(HomeActivity.this, 5000, 1);
+                //Intent intent = new Intent(HomeActivity.this, DebugInterface.class);
+                //startActivity(intent);
             }
         });
 
@@ -242,6 +251,49 @@ public class HomeActivity extends Activity{
             ImageView imageView = (ImageView) findViewById(R.id.pet_round_image);
             imageView.setImageBitmap(bitmap);
         }
+    }
+
+
+    //TESTE//
+
+
+    public void scheduleNotification(Context context, long delay, int notificationId) {//delay is after how much time(in millis) from current time you want to schedule the notification
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setSmallIcon(R.drawable.paw_badge)
+                .setContentTitle("Pet Pet")
+                .setContentText("Your pet requires your attention!")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(
+                        PendingIntent.getActivity(
+                                context,
+                                0,
+                                new Intent(context, HomeActivity.class).putExtra("showAlertDialog", 5), PendingIntent.FLAG_UPDATE_CURRENT)
+                )
+                .setAutoCancel(true);
+
+        Intent intent = new Intent(context, HomeActivity.class).putExtra("showAlertDialog", 5);
+        PendingIntent activity = PendingIntent.getActivity(context, notificationId, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+        builder.setContentIntent(activity);
+
+        Notification notification = builder.build();
+
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, 13);
+        calendar.set(Calendar.MINUTE, 30);
+        calendar.set(Calendar.SECOND, 0);
+
+        Intent notificationIntent = new Intent(context, NotificationPublisher.class);
+        notificationIntent.putExtra(NotificationPublisher.NOTIFICATION_ID, notificationId);
+        notificationIntent.putExtra(NotificationPublisher.NOTIFICATION, notification);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, notificationId, notificationIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+        long futureInMillis = SystemClock.elapsedRealtime() + delay;
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        alarmManager.setRepeating(alarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),AlarmManager.INTERVAL_DAY,pendingIntent);
+        //--Acima é repetir, abaixo é só um alarme.
+        // alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, futureInMillis, pendingIntent);
     }
 
 }//Fim do Arquivo.
